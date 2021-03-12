abdf9bebabf1d19956de50e7c8a16333e026044b
mikecrbenton <mikecrbenton@hotmail.com>
2021-03-12 15:49:29 -0600

    find() working

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,9 @@
 const db = require('../../data/db-config.js');
 
 function find() {
+   return db("users as u")
+   .innerJoin("roles as r", "r.role_id", "u.role_id")
+   .select("u.user_id", "u.username", "r.role_name as role")
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
